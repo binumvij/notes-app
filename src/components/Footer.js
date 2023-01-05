0c0ce6d1f31bf461f285f8d0854e13bf825fdc42
Binu M Vijayan <binumvij68@gmail.com>
2023-01-05 22:43:05 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <a href="https://github.com/binumvij">@binumvij</a>
+        </div>
+    );
+}
+
+export default Footer;
